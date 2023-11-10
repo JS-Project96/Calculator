a859cc0e29d89c8574b387725e734ba1b5d38673
JS-Project96 <jshorter206854@gmail.com>
2023-11-10 12:02:40 +0000

    added basic functionality to update calculator display

--- js/script.js
+++ js/script.js
@@ -14,10 +14,21 @@ function divide(a, b) {
     return a / b;
 }
 
-let firstNumber
-let operator
-let secondNumber
+let firstNumber;
+let operator;
+let secondNumber;
+let displayValue = '';
 
 function operate(firstNumber, secondNumber, operator){
     return operator(firstNumber, secondNumber);
-}
+};
+
+const calcButtons = document.querySelectorAll('.numbers');
+const display = document.querySelector('.display');
+
+for (let i=0; i < calcButtons.length; i++) {
+    calcButtons[i].addEventListener('click', () => {
+        displayValue += calcButtons[i].textContent;
+        display.textContent = displayValue;
+    })
+};
